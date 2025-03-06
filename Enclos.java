26c19724b7187d04fba6d0d47903c9e438d26369
Nico <ntorchaire@weareloom.fr>
2025-03-06 13:17:23 +0100

    Compil

---
+++ Enclos.java
@@ -0,0 +1,12 @@
+class Enclos {
+	List<Pingouins> pingouins = new ArrayList();
+
+	Enclos(List<Pingouins> pingouins) {
+		this.pingouins.addAll(pingouins);
+	}
+
+	void addPingouin(Pingouin pingouin) {
+		pingouins.add(pingouin);
+	}
+	
+}
---
+++ Pingouin.java
@@ -0,0 +1,19 @@
+class Pingouin {
+
+String nom;
+Sexe sexe;
+String couleur;
+
+Pingouin(String nom, Sexe sexe, String couleur) {
+	this.nom = nom;
+	this.sexe = sexe;
+	this.couleur = couleur;
+}
+
+String getCouleur() {
+return couleur;
+}
+
+String getSexe() {
+return sexe;
+}
